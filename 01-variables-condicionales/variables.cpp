15e2d25895f11be3ec4b678deb2ae98979a3c9db
Gabriel Cabrera <gabrielcabreraocola@MacBook-Pro-de-Gabriel.fritz.box>
2024-03-02 16:52:54 +0100

    Done

---
+++ 01-variables-condicionales/variables.cpp
@@ -0,0 +1,25 @@
+#include <iostream> // #include <bits/stdc++.h>
+using namespace std;
+int main(){
+    // Variables
+    // Booleans
+    bool p = true;
+    bool q = false;
+    // Numeros
+    int y;
+    y = 18;
+    int x = 16;
+    x /= 4;
+    float z = 1.5;
+    z += 10;
+    z *= 2;
+    z /= 3;
+    // Caracteres y textos
+    char c = 'e';
+    string cad = "Hola a todos";
+    cad = cad + ", soy Pedro";
+    cout << cad << "\n";
+    cout << x << endl; // endl = "\n"
+    cout << z << endl;
+    return 0;
+}
